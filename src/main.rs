f7708363460a2ca0af71c020f36a9296a9e6a90b
necocen <necocen@gmail.com>
2022-09-27 23:56:05 +0900

    chore(deps): update rustkbd

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use embedded_hal::watchdog::{Watchdog as _, WatchdogEnable};
 use embedded_time::{
     duration::Extensions as _, fixed_point::FixedPoint as _, rate::Extensions as _,
 };
-use layout::{Layer, Layout};
+use layout::Layout;
 use panic_probe as _;
 use rp_pico::{
     entry,
@@ -43,7 +43,6 @@ type KeyboardType = Controller<
     12,
     UsbCommunicator<'static, UsbBus>,
     KeyMatrix<Delay, Pin<Gpio26, FloatingInput>, 4, 4, 12>,
-    Layer,
     Layout,
 >;
 static mut KEYBOARD: Mutex<RefCell<Option<KeyboardType>>> = Mutex::new(RefCell::new(None));
